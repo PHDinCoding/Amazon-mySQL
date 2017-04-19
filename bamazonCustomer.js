ce7d69e8b6d86ae2fd7897d13b72f59a1ef7064f
Aashish Panvelkar <Aashish Panvelkar>
2017-04-18 22:47:36 -0400

    updating pass

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,8 +7,8 @@ var connection = mysql.createConnection({
     port: 3306,
 
     user: "root",
-
-    password: "sun123",
+    //Input your pass please
+    password: "",
     database: "bamazon"
 });
 
